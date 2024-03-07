85a335ae464a07f17dbd73bce48e2f8863fb7cbd
Min-Haw, Liu <liuminhaw@gmail.com>
2024-03-08 01:05:55 +0800

    feat: update i3 scripts for monitor and brightness

--- i3/scripts/auto-detect.sh
+++ i3/scripts/auto-detect.sh
@@ -2,24 +2,28 @@
 # This script is used to automatically detect and configure the displays
 # Note: use `xrandr -q` to get the display names
 
+_BUILTIN_DISPLAY="eDP"
+_BUILTIN_DISPLAY_RESOLUTION="1920x1200"
+_EXTERNAL_DISPLAY_RESOLUTION="1920x1080"
+
 if [[ ${#} -eq 1 && "${1}" == "normal" ]]; then
     _detected_displays=()
 else
-    read -r -d ' ' -a _detected_displays <<< "$(xrandr -q | grep " connected " | grep -v "eDP-1" | cut -d ' ' -f1)"
+    read -r -d ' ' -a _detected_displays <<< "$(xrandr -q | grep " connected " | grep -v "${_BUILTIN_DISPLAY}" | cut -d ' ' -f1)"
 fi
-read -r -d ' ' -a _displays <<< "$(xrandr -q | grep "connected" | grep -v "eDP-1" | cut -d ' ' -f1)"
+read -r -d ' ' -a _displays <<< "$(xrandr -q | grep "connected" | grep -v "${_BUILTIN_DISPLAY}" | cut -d ' ' -f1)"
 
 if [[ ${#_detected_displays[@]} -eq 1 ]]; then
-    _arguments="--output eDP-1 --off"
+    _arguments="--output ${_BUILTIN_DISPLAY} --off"
     for _display in "${_displays[@]}"; do
         if [[ "${_display}" == "${_detected_displays[0]}" ]]; then
-            _arguments="${_arguments} --output ${_display} --primary --mode 1920x1080 --pos 0x0 --rotate normal"
+            _arguments="${_arguments} --output ${_display} --primary --mode ${_EXTERNAL_DISPLAY_RESOLUTION} --pos 0x0 --rotate normal"
         else
             _arguments="${_arguments} --output ${_display} --off"
         fi
     done
 else
-    _arguments="--output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal"
+    _arguments="--output ${_BUILTIN_DISPLAY} --mode ${_BUILTIN_DISPLAY_RESOLUTION} --pos 0x0 --rotate normal"
     for _display in "${_displays[@]}"; do
         _arguments="${_arguments} --output ${_display} --off"
     done
