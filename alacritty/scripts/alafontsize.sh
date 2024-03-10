f560602b3498310da560af0e0e9af743d16cd9f1
Min-Haw, Liu <liuminhaw@gmail.com>
2024-03-10 13:40:45 +0800

    feat: update alacritty alafontsize script with get / set function

--- alacritty/scripts/alafontsize.sh
+++ alacritty/scripts/alafontsize.sh
@@ -4,33 +4,82 @@
 
 _ALACRITTY_CONFIG="${HOME}/.config/alacritty/alacritty.yml"
 
-# Check user input
-if [[ ${#} -ne 1 ]]; then
-    echo "Usage: alfontsize.sh <font size>"
-    exit 1
-fi
-_font_size=${1}
+# -------------------------------------
+# Show script usage
+# Output:
+#  Write usage information to stdout
+# -------------------------------------
+show_help() {
+	cat <<EOF
+Usage: 
+    ${0##*/} get
+    ${0##*/} set <font size>
+EOF
+}
 
-# Check if the font size is a number
-if [[ ! ${_font_size} =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
-    echo "The font size must be a number"
-    exit 1
-fi
+# ------------------------------------------------------------------------
+# Get and show the current font size from alacritty config file
+# Globals:
+#  _ALACRITTY_CONFIG
+# Output:
+#   Write the current font size to stdout
+# ------------------------------------------------------------------------
+get_font_size() {
+	local _font_size=$(grep "  size: " ${_ALACRITTY_CONFIG} | awk '{print $2}')
+	echo "Current font size: ${_font_size}"
+}
 
-# Check if font size setup exist in the file
-if [[ $(grep -c "  size: " ${_ALACRITTY_CONFIG}) -ne 1 ]]; then
-    echo "The font size setup does not exist in the file or exist multiple times"
-    exit 1
-fi
+# ------------------------------------------------------------------------
+# Set font size to given value
+# Globals:
+#  _ALACRITTY_CONFIG
+# Arguments:
+#  $1: font size
+# Output:
+#  Write modified result to stdout
+# ------------------------------------------------------------------------
+set_font_size() {
+	if [[ ${#} -ne 1 ]]; then
+		echo "[ERROR] Function ${FUNCNAME[0]}: invalid numver of arguments"
+		exit 1
+	fi
 
-# Backup config file
-cp ${_ALACRITTY_CONFIG} ${_ALACRITTY_CONFIG}.bak
+	local _font_size=${1}
 
-# Change font size
-sed -E "s/  size: [0-9]*\.?[0-9]*/  size: ${_font_size}/" ${_ALACRITTY_CONFIG} > ${_ALACRITTY_CONFIG}.tmp
-cat ${_ALACRITTY_CONFIG}.tmp > ${_ALACRITTY_CONFIG}
+	# Check if the font size is a number
+	if [[ ! ${_font_size} =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+		echo "[ERROR] Function ${FUNCNAME[0]}: input font size is not a number"
+		exit 1
+	fi
 
-echo "Font size changed to ${_font_size}"
+	# Check if font size setup exist in the file
+	if [[ $(grep -c "  size: " ${_ALACRITTY_CONFIG}) -ne 1 ]]; then
+		echo "[ERROR] Function ${FUNCNAME[0]}: font size setup does not exist in config"
+		exit 1
+	fi
 
-# Remove temporary file
-rm ${_ALACRITTY_CONFIG}.tmp
+	# Backup config file
+	cp ${_ALACRITTY_CONFIG} ${_ALACRITTY_CONFIG}.bak
+
+	# Change font size
+	sed -E "s/  size: [0-9]*\.?[0-9]*/  size: ${_font_size}/" ${_ALACRITTY_CONFIG} >${_ALACRITTY_CONFIG}.tmp
+	cat ${_ALACRITTY_CONFIG}.tmp >${_ALACRITTY_CONFIG}
+
+	# Remove temporary file
+	rm ${_ALACRITTY_CONFIG}.tmp
+
+	echo "Font size changed to ${_font_size}"
+}
+
+main() {
+	if [[ ${#} -eq 1 && ${1} == "get" ]]; then
+		get_font_size
+	elif [[ ${#} -eq 2 && ${1} == "set" ]]; then
+		set_font_size ${2}
+	else
+		show_help
+		exit 1
+	fi
+}
+
+main ${@}
