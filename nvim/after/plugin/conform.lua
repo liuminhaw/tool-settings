e954a313e254605b879132666a273a185e06da83
Min-Haw, Liu <liuminhaw@gmail.com>
2024-03-14 00:24:49 +0800

    feat: nvim conform set deno format with 4 space

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -1,18 +1,37 @@
 require("conform").setup({
-    formatters_by_ft = {
-        sh = { "shfmt" },
-        lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        go = { "gofumpt", "goimports", "golines" },
-        -- Use a sub-list to run only the first available formatter
-        javascript = { "deno_fmt" },
-        markdown = { "deno_fmt" },
-        json = { "deno_fmt" },
-        html = { "prettier" },
-        yaml = { "prettier" },
-    },
+	formatters_by_ft = {
+		sh = { "shfmt" },
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		-- python = { "isort", "black" },
+		go = { "gofumpt", "goimports", "golines" },
+		-- Use a sub-list to run only the first available formatter
+		javascript = { "deno_fmt" },
+		markdown = { "deno_fmt" },
+		json = { "deno_fmt" },
+		html = { "prettier" },
+		yaml = { "prettier" },
+		sql = { "sqlfmt" },
+	},
+	formatters = {
+		deno_fmt = {
+			args = function(self, ctx)
+				return {
+					"fmt",
+					"-",
+					"--indent-width",
+					4,
+					"--ext",
+					vim.fn.expand("%:e"),
+				}
+			end,
+		},
+	},
 })
 
-vim.keymap.set('n', '<leader>f', ':lua require("conform").format({ bufnr = vim.api.nvim_get_current_buf() })<CR>',
-    { noremap = true, silent = true })
+vim.keymap.set(
+	"n",
+	"<leader>f",
+	':lua require("conform").format({ bufnr = vim.api.nvim_get_current_buf() })<CR>',
+	{ noremap = true, silent = true }
+)
